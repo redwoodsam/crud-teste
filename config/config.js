b7e8b17ecc5c1c04089bb2a52b52a0dd48d11801
Samuel Araujo <74160906+redwoodsam@users.noreply.github.com>
2022-12-05 19:41:23 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -4,20 +4,23 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: "mysql"
+    dialect: "mysql",
+    dialectModule: require('mysql2')
   },
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: "mysql"
+    dialect: "mysql",
+    dialectModule: require('mysql2')
   },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: "mysql"
+    dialect: "mysql",
+    dialectModule: require('mysql2')
   }
 }
